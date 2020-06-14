dd153ec91579dae2b6e87302ab49ab6482a4282d
Filip <filipgar@kth.se>
2020-06-14 15:01:02 +0200

    base for work is complete

--- functions/test/test.js
+++ functions/test/test.js
@@ -3,7 +3,12 @@ const path = require('path');
 const fs = require('fs');
 
 describe('Files exists', () => {
-    it('index.html exists', () => { 
+    it('index.html exists in functions', () => { 
         assert.equal(fs.existsSync(path.resolve('./index.html')),true);
     });
 });
+describe('Files do not exist', () => {
+    it('index.html does not exist in build', () => { 
+        assert.notEqual(fs.existsSync(path.resolve('./../build/index.html')),true);
+    });
+})
