eff22310ef8245f9d85d843c5b0282c9d039788f
Filip <filipgar@kth.se>
2020-06-17 15:05:16 +0200

    fix markdown?

--- functions/index.js
+++ functions/index.js
@@ -7,6 +7,7 @@ const express  = require('express');
 const app = express();
 const fs = require('fs');
 const path = require('path');
+const ReactMarkdown = require('react-markdown');
 
 
 app.get('**', (req, res) => {
