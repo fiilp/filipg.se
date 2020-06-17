f29b132a2d05ba8f501390b0f4f106dd2e50b013
Filip <filipgar@kth.se>
2020-06-17 18:27:48 +0200

    style and structure changes to AboutMe > ContentBox

--- src/views/aboutMe/aboutMe.js
+++ src/views/aboutMe/aboutMe.js
@@ -5,13 +5,13 @@ import './aboutMe.css';
 const text1 = () =>
 `Under min gymnasietid studerade jag informations- och mediateknik med intriktning spelutveckling på NTI-Gymnasiet
 på Odenplan. Efter det spenderade jag två perioder på BTH där jag läste Software Engineering men kände att BTH inte passade mig.
-Jag tänkte att om jag ändå skulle läsa ett ingenjörsprogram kunde jag lika gärna göra det på KTH.  
-Sedan dess har jag studerat på högskoleingejörsprogrammet för datateknik på KTH.\n
+Jag tänkte att om jag ändå skulle läsa ett ingenjörsprogram kunde jag lika gärna göra det på KTH. Sedan dess har jag studerat på
+högskoleingejörsprogrammet för datateknik på KTH.\n
 Under senaste tiden har jag under min fridtid lekt runt med olika programmeringsspårk så som; C#, Java och Javascript.
 Just nu testar jag framförallt olika tekniker i JavaScript. Den här hemsidan bl.a. gjordes i ett försök att pröva på **Server Side Rendering** med **React**.
 `;
 const text2 = () =>
-`### Utbildning  
+`### Studier
 Typ | Plats | Utbildning | Längd
 --- | --- | --- | ---
 Högskola | KTH, Stockholm | Datateknik | 2018 -
@@ -23,7 +23,15 @@ Gymnasial | NTI-gymnasiet, Odenplan | Informations- och mediateknik | 2014 - 201
 * Tyska - kan tala
 * Ungerska - kan förstå
 `;
-
+const text3 = () =>
+`### Tidigare jobb
+Företag | Roll | Period 
+--- | --- | --- 
+Wincher | Chattsupport & Utvecklare | 2019-
+Tobii Dynavox | Översättning (engelska till svenska), publicering av nya varor på hemsida | 2017
+Spel & Sånt | Kundservice, DVD-slipning, publicering av nya varor på hemsidan | 2013
+Konsum | Placering av varor, städ och inventering | 2012
+`;
 
 function AboutMe(props){
     return (
@@ -36,10 +44,17 @@ function AboutMe(props){
                 />
             </div>
             <div>
-                <h2>Kunskaper & Erfarenheter</h2>
+                <h2>Utbildning</h2>
+                <ContentBox
+                    img={{alt: 'Utveckling med lugnande dryck.', src: './skola.png'}}
+                    markdown={text2()}
+                />
+            </div>
+            <div>
+                <h2>Erfarenheter</h2>
                 <ContentBox
                     img={{alt: 'Utveckling med lugnande dryck.', src: './wurk.png'}}
-                    markdown={text2()}
+                    markdown={text3()}
                 />
             </div>
         </div>
